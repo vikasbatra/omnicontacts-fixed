5e3abee08c156ac620fceb628efa242f7604e799
Vikas Batra <vikas@calipus.com>
2014-10-16 15:06:21 +0530

    access token in DB

--- lib/omnicontacts/middleware/oauth2.rb
+++ lib/omnicontacts/middleware/oauth2.rb
@@ -52,6 +52,7 @@ module OmniContacts
                                                       end
           contacts = fetch_contacts_using_access_token(access_token, token_type)
           session[refresh_token_prop_name(code)] = refresh_token if refresh_token
+          session[:omnicontacts_access_token] = access_token
           contacts
         else
           raise AuthorizationError.new("User did not grant access to contacts list")
